c5de93c2530d53299701676292ebe0a5d074f78a
Judicael45 <117268223+Judicael45@users.noreply.github.com>
2022-11-21 14:01:54 +0100

    Storage

---
+++ 04-javascript/Storage/Storageexo.js
@@ -0,0 +1,41 @@
+"use strict"
+
+const red = document.querySelector("#redTheme")
+const yellow = document.querySelector("#yellowTheme")
+const green = document.querySelector("#greenTheme")
+//red.addEventListener("input",changeTheme)
+
+function changeTheme() {
+    document.body.classList.toggle("red", red.checked);
+}
+
+red.addEventListener("input", changeTheme2);
+yellow.addEventListener("input", changeTheme2);
+green.addEventListener("input", changeTheme2);
+
+function changeTheme2() {
+    if (red.checked) {
+        // documentElement représenter l'élément racine.
+        document.documentElement.style.setProperty("--fond", "red")
+        document.documentElement.style.setProperty("--text", "black")
+        // Je sauvegarde mon thème sombre
+        localStorage.setItem("theme", "red")
+        red.checked = true
+    }
+    else if(yellow.checked) {
+        document.documentElement.style.setProperty("--fond", "yellow")
+        document.documentElement.style.setProperty("--text", "black")
+        // Je retire mon thème sombre
+        localStorage.setItem("theme", "yellow")
+    }
+    else if(green.checked){
+        document.documentElement.style.setProperty("--fond", "green")
+        document.documentElement.style.setProperty("--text", "black")
+        // Je retire mon thème sombre
+        localStorage.setItem("theme", "green")
+    }
+}
+red.checked = localStorage.getItem("theme") === "red"
+yellow.checked = localStorage.getItem("theme") === "yellow"
+green.checked = localStorage.getItem("theme") === "green"
+changeTheme2()

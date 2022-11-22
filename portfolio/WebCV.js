26ccb2d976501fae68a965b414c5ad5fdb364405
Judicael45 <117268223+Judicael45@users.noreply.github.com>
2022-11-22 16:53:26 +0100

    portfolio

--- portfolio/WebCV.js
+++ portfolio/WebCV.js
@@ -1,2 +1,24 @@
 "use strict"
 
+const black = document.querySelector("#blackTheme")
+const white = document.querySelector("#whiteTheme")
+black.addEventListener("input", changeTheme2);
+white.addEventListener("input",changeTheme2);
+
+function changeTheme2() {
+    if (black.checked) {
+        document.documentElement.style.setProperty("--font", "black")
+        document.documentElement.style.setProperty("--text", "white")
+        localStorage.setItem("theme", "black")
+        black.checked = true
+    }
+    else if(white.checked){
+        document.documentElement.style.setProperty("--font", "white")
+        document.documentElement.style.setProperty("--text", "black")
+        localStorage.setItem("theme", "white")
+        
+    }
+}
+
+black.checked = localStorage.getItem("theme") === "black"
+white.checked = localStorage.getItem("theme") === "white"

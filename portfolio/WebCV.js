4f1ba937b8fb628728945ada9ec23f7aa00c3448
Judicael45 <117268223+Judicael45@users.noreply.github.com>
2022-11-24 16:57:19 +0100

    portfolio

--- portfolio/WebCV.js
+++ portfolio/WebCV.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 const black = document.querySelector("#blackTheme")
 const white = document.querySelector("#whiteTheme")
 black.addEventListener("input", changeTheme2);
@@ -8,18 +7,21 @@ white.addEventListener("input",changeTheme2);
 
 function changeTheme2() {
     if (black.checked) {
-        document.documentElement.style.setProperty("--font", "black")
-        document.documentElement.style.setProperty("--text", "white")
-        localStorage.setItem("theme", "black")
+        document.documentElement.style.setProperty("--font", "rgb(20, 20, 20)")//Thème Noir
+        document.documentElement.style.setProperty("--text", "rgba(255, 255, 255, 0.788)")//Thème blanc
+        localStorage.setItem("theme", "rgb(20, 20, 20)")
         black.checked = true
     }
     else if(white.checked){
-        document.documentElement.style.setProperty("--font", "white")
-        document.documentElement.style.setProperty("--text", "black")
-        localStorage.setItem("theme", "white")
+        document.documentElement.style.setProperty("--font", "rgba(255, 255, 255, 0.788)")
+        document.documentElement.style.setProperty("--text", "rgb(20, 20, 20)")
+        localStorage.setItem("theme", "rgba(255, 255, 255, 0.788)")
         
     }
 }
 
-black.checked = localStorage.getItem("theme") === "black"
-white.checked = localStorage.getItem("theme") === "white"
+black.checked = localStorage.getItem("theme") === "rgb(20, 20, 20)"
+white.checked = localStorage.getItem("theme") === "rgba(255, 255, 255, 0.788)"
+
+
+

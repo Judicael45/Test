a1bb6c3909a951efe0d384f4a42cd028d6ec5f57
Judicael45 <117268223+Judicael45@users.noreply.github.com>
2022-12-01 16:56:52 +0100

    cours

---
+++ 04-javascript/Module/Exercice Module/slider.js
@@ -0,0 +1,5 @@
+"use strict"
+
+import * as showSlides from "./script.js"
+
+console.log(showSlides);
---
+++ 04-javascript/Module/Salutation.js
@@ -0,0 +1,36 @@
+"use strict"
+
+/** 
+*Affiche un message de salutation  dans la console.
+* @return{void}
+*/
+export default function bonjour()
+{
+    console.log("Bonjour tout le monde !");
+}
+export function salut ()
+{
+    console.log("Salut la population !");
+}
+/**
+ * Affiche un message de salutation dans la console.
+ * @param {string} name nom de la personne qui parle.
+ */
+export function coucou(name)
+{
+    parler(name,"Coucou tout le monde")
+}
+
+/**
+ * Afficher un message dans la console précédé du nom de la personne .
+ * @param {string} nom Nom de la personne qui parle
+ * @param {string} text Message de la personne qui parle
+ */
+
+
+function parler(nom, text)
+{
+    console.log(`${nom} : ${text}`);
+}
+
+console.log("Salutation importé");
